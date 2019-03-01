2bde11f970362d8a3beda7138ab37eca2d0ff897
Koushik Kumaran <koushikkumaran52@gmail.com>
2019-03-01 20:44:21 +0530

    Removed Idiocy

--- Orthosis_control/Orthosis_control.ino
+++ Orthosis_control/Orthosis_control.ino
@@ -1,35 +1,50 @@
-//I=Inlet;O=Outlet
-int valves[2][2] = {{12,13},{14,15}};
+//I=Inlet; O=Outlet
 
-void setup()
-{
-  for(int i=0 ; i<2 ; i++)
-    for(int j=0 ; j<2 ; j++)
-      valves[i][j] = 0;
-}
+int valves[2][2] = {{9,10},{11,12}};
 
 void inflate(int n)
 {
  digitalWrite(valves[n+1][0],HIGH);
  digitalWrite(valves[n+1][1],LOW); 
 }
+
 void exhaust(int n)
 {
  digitalWrite(valves[n+1][0],LOW);
  digitalWrite(valves[n+1][1],HIGH); 
 }
+
 void hold(int n)
 {
  digitalWrite(valves[n+1][0],LOW);
  digitalWrite(valves[n+1][1],LOW) ;
 }
+
 void hold_all()
 {
  hold(1);
  hold(2); 
 }
 
+void setup()
+{
+  hold_all();
+}
+
 void loop() 
 {
-                         
+  inflate(1);
+  delay(5000);
+  exhaust(1);
+  delay(2500);
+  inflate(2);
+  delay(5000);
+  exhaust(2);
+  delay(2500);
+  inflate(1);
+  inflate(2);
+  hold_all();
+  exhaust(1);
+  exhaust(2);
+  delay(2500);                         
 }
